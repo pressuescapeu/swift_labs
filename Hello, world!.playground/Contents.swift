8ef70ce502afe0e2e7a166953390193be68d63df
pressuescapeu <99085281+pressuescapeu@users.noreply.github.com>
2024-07-15 20:37:37 +0500

    Add files via upload

---
+++ Hello, world!.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var greeting = "Hello, playground"
+print("Я сегодня изучил: ")
+print("Основы языка Swift")
+print("Как использовать Playgrounds")
+print("свои собственные фразы в консоль: ")
+print(greeting)
+print("wagwan")
